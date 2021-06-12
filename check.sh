ad03210f56b7a960066abdc8a6aa8c5884850122
Robert Bohne <robert.bohne@redhat.com>
2021-06-12 12:57:53 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-OBJECTS="$(cat objects.list) "
+#OBJECTS="$(cat objects.list) "
 
-for O in $OBJECTS ; do
-    printf "%-110s " $O 
+for O in $(cat $1) ; do
+    printf "%-150s " $O
     kubectl get -o name $O 2>/dev/null 1>/dev/null
     if [ $? == 1 ]; then
         echo " MISSSING!"
